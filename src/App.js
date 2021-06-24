0d50104d50fa50343641ef664297ff511c119427
Frank Foo <frankfoo37@gmail.com>
2021-06-24 18:28:32 +1000

    Allowing special chars in quote + more styling

--- src/App.js
+++ src/App.js
@@ -21,17 +21,17 @@ function App() {
       .then((data) => {
         let newQuote = data.content;
         newQuote = newQuote.toUpperCase();
-        newQuote = newQuote.replaceAll(/[.;',:]/g, '');
+        //newQuote = newQuote.replaceAll(/[.;',:]/g, '');
         setQuote(newQuote.split("")); // We've cleaned the original quote, set it to 'quote'
 
         let re = /[A-Z]/;
-        let re1 = /[.;',:]/;
+        //let re1 = /[.;',:]/;
         let cipherQuote = '';
 
         for (let i = 0; i < newQuote.length; i++) {
           if (re.test(newQuote[i])) {
             cipherQuote += mapping[newQuote[i]]
-          } else if (re1.test(newQuote[i]) === false) {
+          } else {
             cipherQuote += newQuote[i]
           }
         }
@@ -59,11 +59,10 @@ function App() {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Substitution Cipher Game</h1>
-      <p>{quote}</p>
       <div className={styles.gameBox}>
         <div className={styles.inputBox}>
           {board.map((item, key) => (
-            (quote[key] !== ' ' &&
+            (/[A-Z]/.test(quote[key]) &&
               <div className={styles.box}>
                 <p>{cipher[key]}</p>
                 <input type="text" className={styles.input} maxLength="1" id={key} value={item} 
@@ -79,7 +78,12 @@ function App() {
                   }
                 }></input>
               </div>
-              ||
+              || /[.;',:]/.test(quote[key]) &&
+              <div className={styles.box}>
+                <p>{quote[key]}</p>
+                <input type="text" className={styles.input} value={quote[key]} disabled></input>
+              </div>
+              || 
               <div className={styles.box}>
                 <p className={styles.hidden}>a</p>
                 <input type="text" className={styles.input} disabled></input>
@@ -90,19 +94,20 @@ function App() {
       </div>
       <div className={styles.controls}>
         <button className={styles.button} onClick={() => setReset(!reset)}>New Game</button>
-        <p>{win && 
-          <p>CORRECT!</p>
-          }
-        </p>
+        {win && 
+          <p className={styles.correct}>CORRECT!</p>
+        }
       </div>
       <div className={styles.gameInfo}>
         <h2 className={styles.gameInfoTitle}>Game Info:</h2>
         <p>A substitution cipher is where each letter of the alphabet is mapped to a random letter, e.g. every A becomes a P, every B becomes an X.</p>
         <p>Your job is to decipher the letters to get the original quote.</p>
-        <p>There are tricks that you can use:</p>
-        <p>Certain letters in English occur more frequently than others e.g. E, T, A, O</p>
-        <p>Do you see 2 letters that appear together? They could be "O", "E" etc...</p>
+        <p>There are a few tricks that you can use:</p>
+        <p>Some letters in English occur more frequently than others e.g. ETAOIN are the top 6 most frequent letters</p>
+        <p>Do you see a single letter? It could be "A" or "I"</p>
+        <p>Do you see 2 letters that appear together? They could be "OO", "EE" etc...</p>
         <p>Do you see apostrophes like xxx'x? That last letter could be a T, S etc...</p>
+        <p>There aren't that many 3 letter words</p>
         <p>Good luck!</p>
       </div>
     </div>
