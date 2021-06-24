2e6b5f9d73a6c2b7aad80dbc5c48ec9817f07551
Frank Foo <frankfoo37@gmail.com>
2021-06-24 17:54:18 +1000

    Some styling done

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ function App() {
   const [board, setBoard] = React.useState([]);
   // Winning state
   const [win, setWin] = React.useState(false);
+  // Reset
+  const [reset, setReset] = React.useState(false);
   React.useEffect(() => {
     fetch("https://api.quotable.io/random")
       .then((data) => data.json())
@@ -42,7 +44,7 @@ function App() {
         setBoard(newBoard);
 
       });
-  }, []);
+  }, [reset]);
 
   React.useEffect(() => {
     let flag = true;
@@ -56,34 +58,54 @@ function App() {
 
   return (
     <div className={styles.container}>
+      <h1 className={styles.title}>Substitution Cipher Game</h1>
       <p>{quote}</p>
-      <p>{win && 
-        <p>You win!</p>
-        }
-      </p>
-      <div className={styles.inputBox}>
-        {board.map((item, key) => (
-          (quote[key] !== ' ' &&
-            <div className={styles.box}>
-              <p>{cipher[key]}</p>
-              <input type="text" className={styles.input} maxLength="1" id={key} value={item} 
-                onChange={(e) => {
-                  let newChar = e.target.value.toUpperCase();
-                  if (validChar(newChar, board) === true) {
-                    setBoard([...updateBoard(e.target.value, key, cipher, board)])
+      <div className={styles.gameBox}>
+        <div className={styles.inputBox}>
+          {board.map((item, key) => (
+            (quote[key] !== ' ' &&
+              <div className={styles.box}>
+                <p>{cipher[key]}</p>
+                <input type="text" className={styles.input} maxLength="1" id={key} value={item} 
+                  onChange={(e) => {
+                    if (e.target.value == '') {
+                      let targetChar = board[key];
+                      setBoard([...updateBoard(targetChar, key, cipher, board, 1)])
+                    }
+                    let newChar = e.target.value.toUpperCase();
+                    if (validChar(newChar, board) === true) {
+                      setBoard([...updateBoard(e.target.value, key, cipher, board, 0)])
+                    }
                   }
-                }
-              }></input>
-            </div>
-            ||
-            <div className={styles.box}>
-              <p>-</p>
-              <input type="text" className={styles.input} disabled></input>
-            </div>
-          )
-        ))}
+                }></input>
+              </div>
+              ||
+              <div className={styles.box}>
+                <p className={styles.hidden}>a</p>
+                <input type="text" className={styles.input} disabled></input>
+              </div>
+            )
+          ))}
+        </div>
       </div>
-    </div >
+      <div className={styles.controls}>
+        <button className={styles.button} onClick={() => setReset(!reset)}>New Game</button>
+        <p>{win && 
+          <p>CORRECT!</p>
+          }
+        </p>
+      </div>
+      <div className={styles.gameInfo}>
+        <h2 className={styles.gameInfoTitle}>Game Info:</h2>
+        <p>A substitution cipher is where each letter of the alphabet is mapped to a random letter, e.g. every A becomes a P, every B becomes an X.</p>
+        <p>Your job is to decipher the letters to get the original quote.</p>
+        <p>There are tricks that you can use:</p>
+        <p>Certain letters in English occur more frequently than others e.g. E, T, A, O</p>
+        <p>Do you see 2 letters that appear together? They could be "O", "E" etc...</p>
+        <p>Do you see apostrophes like xxx'x? That last letter could be a T, S etc...</p>
+        <p>Good luck!</p>
+      </div>
+    </div>
   );
 }
 
--- src/updateBoard.js
+++ src/updateBoard.js
@@ -1,19 +1,31 @@
 import React from "react";
 
-const updateBoard = (char, key, cipher, board) => {
+const updateBoard = (char, key, cipher, board, flag) => {
     // Char is the character the user has entered 
     // Key is the index
     // Data is the array that contains the cipher quote 
     // Board is the array that contains the user answer 
     // Setboard is the function that changes Board
+    // Flag determines if we're adding a letter or removing it from the board 
     
-    let mappedKey = cipher[key]; // Doesn't matter if we're using the original quote or ciper quote
-    for (let i = 0; i < cipher.length; i++) {
-        if (cipher[i] === mappedKey) {
-            board[i] = char.toUpperCase();
+    // Add the letter in
+    if (flag === 0) {
+        let mappedKey = cipher[key]; // Doesn't matter if we're using the original quote or ciper quote
+        for (let i = 0; i < cipher.length; i++) {
+            if (cipher[i] === mappedKey) {
+                board[i] = char.toUpperCase();
+            }
         }
     }
-    //console.log(board);
+
+    // Remove the letter
+    if (flag === 1) {
+        for (let i = 0; i < board.length; i++) {
+            if (board[i] == char) {
+                board[i] = '';
+            }
+        }
+    }
     return board;
 }
 
